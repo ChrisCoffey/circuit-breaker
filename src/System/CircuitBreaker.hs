6d5c5719b99bb061fb560f30d5202af651c3c984
Chris Coffey <cmcof273@gmail.com>
2019-03-03 17:24:47 -0500

    A simplistic circuit breaker

--- src/System/CircuitBreaker.hs
+++ src/System/CircuitBreaker.hs
@@ -1,19 +1,21 @@
 module System.CircuitBreaker (
 ) where
 
+import Control.Monad.Trans (liftIO)
 import Control.Monad.IO.Unlift (MonadUnliftIO)
 import Control.Monad.Reader (MonadReader, asks)
-import Data.Time.Clock (UTCTime, getCurrentTime)
+import Data.Maybe (fromMaybe, isNothing)
+import Data.Time.Clock (UTCTime, getCurrentTime, addUTCTime)
 import Numeric.Natural (Natural)
 import GHC.TypeLits (Symbol, KnownSymbol, symbolVal, Nat, KnownNat, natVal)
 import Data.Proxy (Proxy(..))
-import UnliftIO.Environment (bracketOnError)
-import UnliftIO.STM (MVar)
+import UnliftIO.Exception (bracketOnError, catchDeep)
+import UnliftIO.MVar (MVar, putMVar, takeMVar, newMVar)
 import qualified Data.Text as T
 import qualified Data.HashMap.Strict as M
 
 newtype CircuitBreakerConf = CBConf {
-    cbBreakers :: M.HashMap T.Text (MVar CircuitState)
+    cbBreakers :: MVar (M.HashMap T.Text (MVar CircuitState))
     }
 
 -- | The current condition of a CircutBreaker may be Active, Testing, or Waiting for the
@@ -52,6 +54,11 @@ data CircutBreakerError
     | CircuitBreakerClosed T.Text
     deriving (Show, Eq, Ord)
 
+data CircutAction
+    = SkipClosed
+    | Run UTCTime
+    deriving (Eq, Show)
+
 -- | Brackets a computation with short-circuit logic. If an uncaught error occurs, then the
 -- circuit breaker opens, which causes all additional threads entering the wrapped code to fail
 -- until the specified timeout has expired. Once the timeout expires, a single thread may enter
@@ -63,8 +70,18 @@ withBreaker :: (Monad m, MonadUnliftIO m, MonadReader env m,
     -> m a
     -> m (Either CircutBreakerError a)
 withBreaker breakerDefinition action = do
-    breakers <- asks getCircuitState
-    bs = undefiend :: CircuitState
+    -- Get the current circut breaker
+    breakerCell <- cbBreakers <$> asks getCircuitState
+    breakers <- takeMVar breakerCell
+    let (lbl, timeout) = reifyCircuitBreaker breakerDefinition
+        mbs = lbl `M.lookup` breakers
+    bs <- maybe (newMVar newBreaker) pure mbs
+
+    -- If it happens to be the first time this block has been called, store the new breakers state
+    if isNothing mbs
+        then putMVar breakerCell $ M.insert lbl bs breakers
+        else  putMVar breakerCell breakers
+
     -- bracketOnError the action
     -- Read the current status & determine whether to perform the action
     --      0) Get the current time
@@ -74,18 +91,48 @@ withBreaker breakerDefinition action = do
     --          4) If the timeout has elapsed, enter 'Testing' state
     --          5) Otherwise, run the computation
     --      6) If the computation fails, set the error time and enter 'Waiting' state
-    bracketOnError before during onError
-    -- It'll be imortant to always keep the MVar full (unless testing) to make sure that it never deadlocks
+    bracketOnError (before bs) (onError bs) (during lbl bs)
     where
         (label, timeout) = reifyCircuitBreaker breakerDefinition
 
-        before = do
+        -- Given the current state of the circuit breaker, determine what action should
+        -- be taken during the body of the 'bracket'.
+        --
+        -- Condition rules:
+        -- 1) If the circuit breaker is in 'Active' then pass the call on
+        -- 2) If the circuit breaker is in 'Testing' then fail the call
+        -- 3) If the circuit breaker is in 'Waiting' and the timeout has not elapsed then fail the call
+        -- 4) If the circuit breaker is in 'Waiting' and the timeout has elapsed then convert to 'Testing' and try the call
+        before bs = do
             now <- liftIO getCurrentTime
+            cb <- takeMVar bs
+            let threshold = addUTCTime (- (fromIntegral timeout)) now
+                elapsed = fromMaybe False $ (threshold >) <$> mostRecentError cb
+            putMVar bs cb
+            case currentState cb of
+                Waiting | elapsed -> do
+                    putMVar bs $ cb {currentState = Testing}
+                    pure $ Run now
+                Active -> do
+                    putMVar bs cb
+                    pure $ Run now
+                Waiting -> do
+                    putMVar bs cb
+                    pure SkipClosed
+                Testing -> do
+                    putMVar bs cb
+                    pure SkipClosed
 
+        during _ _ (Run _) =
+            Right <$> action
+        during lbl bs SkipClosed =
+            pure . Left $ CircuitBreakerClosed lbl
 
-        during
+        -- In the event of an uncaught error during the bracketed computation, flip the circuit breaker to 'Waiting'
+        onError bs (Run ts) = do
+            bs' <- takeMVar bs
+            putMVar bs $ CircuitState {mostRecentError = Just ts, currentState = Waiting}
 
-        onError
 
 newBreaker :: CircuitState
 newBreaker = CircuitState {
